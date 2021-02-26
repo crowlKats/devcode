e7a6b890e72ead8d292691be036dd3eb04fa34b5
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-02-26 10:40:29 +0100

    add basic tests

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod renderer;
 
 use std::collections::HashMap;
 use std::path::PathBuf;
-use wgpu_glyph::ab_glyph;
 use winit::event::WindowEvent;
 
 fn main() -> Result<(), anyhow::Error> {
@@ -21,64 +20,7 @@ fn main() -> Result<(), anyhow::Error> {
   }
   let text = std::fs::read_to_string(filepath)?;
 
-  let font = {
-    let fonts = {
-      #[cfg(target_os = "macos")]
-      {
-        let path = std::path::Path::new("/Library/Fonts/");
-        let mut fonts = std::fs::read_dir(path)?.collect::<Vec<_>>();
-        let path = std::path::Path::new("/System/Library/Fonts/");
-        fonts.extend(std::fs::read_dir(path)?);
-        let expanded_path = shellexpand::tilde("~/Library/Fonts");
-        let expanded_path = expanded_path.to_string();
-        let path = std::path::Path::new(&expanded_path);
-        fonts.extend(std::fs::read_dir(path)?);
-        fonts
-      }
-      #[cfg(target_os = "windows")]
-      {
-        let path = std::path::Path::new(r"C:\Windows\Fonts");
-        std::fs::read_dir(path)?.collect::<Vec<_>>()
-      }
-      #[cfg(target_os = "linux")]
-      {
-        let path = std::path::Path::new("/usr/share/fonts");
-        let mut fonts = std::fs::read_dir(path)?.collect::<Vec<_>>();
-        let path = std::path::Path::new("/usr/local/share/fonts");
-        fonts.extend(std::fs::read_dir(path)?);
-        let path = std::path::Path::new("~/.fonts");
-        fonts.extend(std::fs::read_dir(path)?);
-        let expanded_path = shellexpand::tilde("~/Library/Fonts");
-        let expanded_path = expanded_path.to_string();
-        let path = std::path::Path::new(&expanded_path);
-        fonts.extend(std::fs::read_dir(path)?);
-        fonts
-      }
-    };
-    let fonts = fonts
-      .iter()
-      .filter(|font| font.as_ref().unwrap().path().is_file())
-      .map(|font| {
-        let font_path = font.as_ref().unwrap().path();
-        (
-          font_path
-            .file_stem()
-            .unwrap()
-            .to_os_string()
-            .into_string()
-            .unwrap(),
-          font_path,
-        )
-      })
-      .collect::<HashMap<String, PathBuf>>();
-    let font = args
-      .get(2)
-      .and_then(|font| fonts.get(font))
-      .unwrap_or_else(|| fonts.values().next().unwrap());
-    let font_data = std::fs::read(font)?;
-
-    ab_glyph::FontArc::try_from_vec(font_data)?
-  };
+  let font = get_font(args.get(2))?;
 
   let event_loop = winit::event_loop::EventLoop::new();
   let mut ren = futures::executor::block_on(async {
@@ -98,4 +40,91 @@ fn main() -> Result<(), anyhow::Error> {
     winit::event::Event::RedrawRequested(_) => ren.redraw().unwrap(),
     _ => *control_flow = winit::event_loop::ControlFlow::Wait,
   });
+}
+
+fn get_font_map() -> Result<HashMap<String, PathBuf>, anyhow::Error> {
+  let fonts = {
+    #[cfg(target_os = "macos")]
+    {
+      let path = std::path::Path::new("/Library/Fonts/");
+      let mut fonts = std::fs::read_dir(path)?.collect::<Vec<_>>();
+      let path = std::path::Path::new("/System/Library/Fonts/");
+      fonts.extend(std::fs::read_dir(path)?);
+      let expanded_path = shellexpand::tilde("~/Library/Fonts");
+      let expanded_path = expanded_path.to_string();
+      let path = std::path::Path::new(&expanded_path);
+      fonts.extend(std::fs::read_dir(path)?);
+      fonts
+    }
+    #[cfg(target_os = "windows")]
+    {
+      let path = std::path::Path::new(r"C:\Windows\Fonts");
+      std::fs::read_dir(path)?.collect::<Vec<_>>()
+    }
+    #[cfg(target_os = "linux")]
+    {
+      let path = std::path::Path::new("/usr/share/fonts");
+      let mut fonts = std::fs::read_dir(path)?.collect::<Vec<_>>();
+      let path = std::path::Path::new("/usr/local/share/fonts");
+      fonts.extend(std::fs::read_dir(path)?);
+      let path = std::path::Path::new("~/.fonts");
+      fonts.extend(std::fs::read_dir(path)?);
+      let expanded_path = shellexpand::tilde("~/Library/Fonts");
+      let expanded_path = expanded_path.to_string();
+      let path = std::path::Path::new(&expanded_path);
+      fonts.extend(std::fs::read_dir(path)?);
+      fonts
+    }
+  };
+  Ok(
+    fonts
+      .iter()
+      .filter(|font| font.as_ref().unwrap().path().is_file())
+      .map(|font| {
+        let font_path = font.as_ref().unwrap().path();
+        (
+          font_path
+            .file_stem()
+            .unwrap()
+            .to_os_string()
+            .into_string()
+            .unwrap(),
+          font_path,
+        )
+      })
+      .collect(),
+  )
+}
+
+fn get_font(
+  name: Option<&String>,
+) -> Result<wgpu_glyph::ab_glyph::FontArc, anyhow::Error> {
+  let fonts = get_font_map()?;
+  let font = name
+    .and_then(|font| fonts.get(font))
+    .unwrap_or_else(|| fonts.values().next().unwrap());
+  let font_data = std::fs::read(font)?;
+
+  Ok(wgpu_glyph::ab_glyph::FontArc::try_from_vec(font_data)?)
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::*;
+
+  #[test]
+  fn font_map_contains() {
+    assert!(get_font_map()
+      .unwrap()
+      .contains_key(&String::from("Helvetica")))
+  }
+
+  #[test]
+  fn get_specific_font() {
+    assert!(get_font(Some(&String::from("Helvetica"))).is_ok())
+  }
+  #[test]
+  fn get_first_font() {
+    assert!(get_font(None).is_ok())
+  }
 }
